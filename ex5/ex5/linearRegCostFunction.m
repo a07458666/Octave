f44e1e7be96c37b1b1948e17951d9e7837cef16b
AndySu <a07458666@gmail.com>
2019-04-19 17:56:31 +0800

    Update ex5 50%

--- ex5/ex5/linearRegCostFunction.m
+++ ex5/ex5/linearRegCostFunction.m
@@ -19,17 +19,15 @@ grad = zeros(size(theta));
 %               You should set J to the cost and grad to the gradient.
 %
 
+% 計算基本的costFunction 加入對theta的懲罰 
 h = X * theta;
-penalty = (lambda / (2 * m))* sum(theta(2:end).^2);
+penalty = (lambda / (2 * m))* sum(theta(2:end, :).^2);
 J = (1 / (2 * m)) * sum((h .- y).^2) + penalty;
 
 
-
-
-
-
-
-
+regression = (lambda/m) .* theta; %計算正規化數值
+regression(1) = 0;     %第一項不用正規化
+grad = (1 / m) * sum((h .- y) .* X)' + regression; %計算梯度(微分J)
 
 % =========================================================================
 
