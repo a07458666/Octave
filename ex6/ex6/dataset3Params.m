d4384a745a086f9e66887cdde5c449f36d9c2329
AndySu <a07458666@gmail.com>
2019-05-10 14:59:17 +0800

    Update ex6 50%

--- ex6/ex6/dataset3Params.m
+++ ex6/ex6/dataset3Params.m
@@ -22,12 +22,23 @@ sigma = 0.3;
 %  Note: You can compute the prediction error using 
 %        mean(double(predictions ~= yval))
 %
+C_group = [0.01, 0.03, 0.1, 0.3, 1,3, 10, 30];
+sigma_group = [0.01, 0.03, 0.1, 0.3, 1,3, 10, 30];
+err_list = zeros(length(C_group) , length(sigma_group));
 
-
-
-
-
-
+for i = 1 : length(C_group)
+  for j = 1 : length(sigma_group)
+    C = C_group(i);
+    sigma = sigma_group(j);
+    model= svmTrain(X, y, C, @(x1, x2) gaussianKernel(x1, x2, sigma));
+    predictions = svmPredict(model, Xval);
+    err_list(i,j) = mean(double(predictions ~= yval));
+  endfor
+endfor
+[maxValue,C_index] = min(min(err_list,[],2))
+[maxValue,sigma_index] = min(min(err_list,[],1))
+C = C_group(C_index);
+sigma = sigma_group(sigma_index);
 
 % =========================================================================
 
